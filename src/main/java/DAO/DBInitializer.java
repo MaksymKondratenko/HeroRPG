58f39735338af5d12cb56b1b4bf9dd5157a03664
Maks <maxym.kondratenko@gmail.com>
2017-08-25 18:30:18 +0200

    implementing spring data jdbcTemplate

--- src/main/java/DAO/DBInitializer.java
+++ src/main/java/DAO/DBInitializer.java
@@ -2,22 +2,19 @@ package DAO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DBInitializer {
 
-    private DataSource dataSource;
-    Connection conn = null;
-    Statement state = null;
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     @Qualifier("dataSource")
-    public void setDataSource(DataSource dataSource){this.dataSource = dataSource;}
+    public void setDataSource(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
     public void init() {
         String sql = "CREATE TABLE `actions` (\n" +
@@ -25,24 +22,9 @@ public class DBInitializer {
                 "  `act` varchar(64) DEFAULT NULL,\n" +
                 "  PRIMARY KEY (`id`)\n" +
                 ") ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1";
-        try {
-            conn = dataSource.getConnection();
-            state = conn.createStatement();
-            state.execute("create database if not exists HeroDB");
-            state.execute("use HeroDB");
-            state.execute("drop table if exists actions");
-            state.execute(sql);
-        } catch (SQLException e){
-            e.printStackTrace();
-        } finally {
-            try {
-                if(conn != null)
-                    conn.close();
-                if(state != null)
-                    state.close();
-            } catch (SQLException e){
-                e.printStackTrace();
-            }
-        }
+        jdbcTemplate.execute("create database if not exists HeroDB");
+        jdbcTemplate.execute("use HeroDB");
+        jdbcTemplate.execute("drop table if exists actions");
+        jdbcTemplate.execute(sql);
     }
 }
--- src/main/java/DAO/MysqlDAO.java
+++ src/main/java/DAO/MysqlDAO.java
@@ -3,75 +3,38 @@ package DAO;
 import hero.Action;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
-import java.sql.*;
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class MysqlDAO implements ActionDAO {
 
-    private DataSource dataSource;
-    Connection con = null;
-    PreparedStatement ps = null;
-
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     @Qualifier("dataSource")
     public void setDataSource(DataSource dataSource){
-        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Override
     public void writeToDB(Action action) {
-        String sql = "insert into Actions (Act) value (?)";
         String arg = action.toString();
-        try {
-            con = dataSource.getConnection();
-            ps = con.prepareStatement(sql);
-            ps.setString(1, arg);
-            int out = ps.executeUpdate();
-        } catch(SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (ps != null)
-                    ps.close();
-                if (con != null)
-                    con.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        this.jdbcTemplate.update("insert into Actions (Act) value (?)", arg);
     }
 
     @Override
     public List<String> readFromDB() {
-        String sql = "select * from actions";
-        Statement state = null;
-        ResultSet rs = null;
-        List<String> ls = new ArrayList<>();
-        try {
-            con = dataSource.getConnection();
-            state = con.createStatement();
-            rs = state.executeQuery(sql);
-            while (rs.next())
-                ls.add(rs.getString("act"));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (con != null)
-                    con.close();
-                if (state != null)
-                    state.close();
-                if (rs != null)
-                    rs.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
+        return this.jdbcTemplate.query("select * from actions", new RowMapper<String>() {
+            @Override
+            public String mapRow(ResultSet resultSet, int i) throws SQLException {
+                String str = resultSet.getString("act");
+                return str;
             }
-        }
-            return ls;
+        });
     }
-
 }
